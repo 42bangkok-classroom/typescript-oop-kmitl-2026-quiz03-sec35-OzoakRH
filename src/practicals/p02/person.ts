406c3c9c84a5d199d5703d8c343b4bdc1246b93e
OzoakRH <kanekineki50@gamil.com>
2026-02-16 09:45:06 +0700

    P02 test2

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -2,14 +2,14 @@ export class Person {
     firstname: string = ''
     lastname: string = ''
     private _age: number = 0
-    static country: string = 'Thailand'
+    static COUNTRY: string = 'Thailand'
     setAge(age:number){
         this._age = age
     }
     getAge(): number {
         return this._age
     }
-    getFullname(): string {
+    getFullName(): string {
         return `${this.firstname} ${this.lastname}`
     }
 }
