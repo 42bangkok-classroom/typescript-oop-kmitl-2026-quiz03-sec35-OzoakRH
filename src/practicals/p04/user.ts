ef525427d9a73a196280c6f350b70670e91b2a7a
OzoakRH <kanekineki50@gamil.com>
2026-02-16 10:12:27 +0700

    P04

--- src/practicals/p04/user.ts
+++ src/practicals/p04/user.ts
@@ -1,1 +1,19 @@
-export class User {}
+export class User {
+    public username: string = ''
+    public password: string = ''
+    public LOGIN_ATTEMPTS: number = 0
+    constructor (username:string, password:string){
+        this.username = username
+        this.password = password
+    }
+    public login(password:string):boolean{
+        this.LOGIN_ATTEMPTS++
+        return this.validPassword(password)
+    }
+    private validPassword(password:string):boolean{
+        return this.password === password
+    }
+    public getLoginAttempts():number{
+        return this.LOGIN_ATTEMPTS
+    }
+}
